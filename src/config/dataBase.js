16bfe0fce3948a207caafd5ad3bace4f56558d7e
shashank <shashankksingh05@gmail.com>
2026-06-13 22:03:06 +0530

    MakeUp added 3 dots in MongoDB connected...

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI)
-    console.log('MongoDB connected')
+    console.log('MongoDB connected...')
     
   } catch (err) {
     console.log('Connection Failed ' , err)
