5b6a16a3e3a971862b6c892baf12ba551bd13bc4
shashank <shashankksingh05@gmail.com>
2026-07-11 19:37:12 +0530

    feat: update protect middleware to read access token from httpOnly cookie

--- syncbeats-backend/src/middlewares/auth.js
+++ syncbeats-backend/src/middlewares/auth.js
@@ -2,20 +2,26 @@ const jwt = require('jsonwebtoken')
 
 
 const protect = (req, res, next) => {
-  const authHeader= req.headers['authorization']
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-  return res.status(401).json({ message: 'Access denied' });
-}
-  const token = authHeader.split(" ")[1]
+  
+  const accessToken = req.cookies.accessToken
+  if (!accessToken) {
+     return res.status(401).json({ message: 'No access token, please login again' })
+   }
     try{
-    const decoded = jwt.verify(token , process.env.JWT_SECRET)
-    req.userId= decoded.userId
+    const decoded = jwt.verify(accessToken , process.env.JWT_SECRET)
+    if (decoded.type !== 'access') {
+     return res.status(401).json({ message: 'Invalid token type' })
+   }
+    req.userId = decoded.userId
     next()
-    } catch(err){
-        res.status(401).json({message: 'Invalid Token'})
+    }catch(err){
+        if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') {
+        return res.status(401).json({ message: 'Invalid or expired refresh token, please login again' })
+     }
+      res.status(500).json({ message: 'Something went wrong!', error: err.message })
     }
 
 }
 
 
-module.exports = protect;
+module.exports = protect; 
